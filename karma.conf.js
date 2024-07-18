4e591f751029dafedd4cf193d715f5e0b5969894
José Bocanegra <josejbocanegra@gmail.com>
2024-07-18 11:36:58 -0500

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       colors: true,
       logLevel: config.LOG_INFO,
       autoWatch: true,
-      browsers: ['Chrome', 'ChromeHeadless', 'ChromeHeadlessCI'],
+      browsers: ['ChromeHeadless', 'Chrome'],
       customLaunchers: {
         ChromeHeadlessCI: {
           base: 'ChromeHeadless',
