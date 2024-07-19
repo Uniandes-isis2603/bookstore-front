966ab1832d5113b38e9e8c3c229ba3565218f94e
Jose Bocanegra <j.bocanegra@uniandes.edu.co>
2024-07-18 20:41:57 -0500

    Fix tests

--- karma.conf.js
+++ karma.conf.js
@@ -1,22 +1,20 @@
 module.exports = function (config) {
-    config.set({
-      basePath: '',
-      frameworks: ['jasmine', '@angular-devkit/build-angular'],
-      plugins: [
-        require('karma-jasmine'),
-        require('karma-firefox-launcher'),
-        require('@angular-devkit/build-angular/plugins/karma')
-      ],
-      client:{
-        clearContext: false
-      },
-      reporters: ['progress'],
-      port: 9876,
-      colors: true,
-      logLevel: config.LOG_INFO,
-      autoWatch: true,
-      browsers: [],
-      captureTimeout: 180000,
-      singleRun: true
-    });
+  config.set({
+    basePath: "",
+    frameworks: ["jasmine", "@angular-devkit/build-angular"],
+    plugins: [
+      require("karma-jasmine"),
+      require("@angular-devkit/build-angular/plugins/karma"),
+    ],
+    client: {
+      clearContext: false,
+    },
+    reporters: ["progress"],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: false,
+    browsers: [ChromeHeadless],
+    singleRun: true,
+  });
 };
