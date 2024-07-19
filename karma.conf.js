0043b1a6776392f8cb7d874b7a05cbc48939f842
Jose Bocanegra <j.bocanegra@uniandes.edu.co>
2024-07-18 20:46:54 -0500

    Fix tests

--- karma.conf.js
+++ karma.conf.js
@@ -1,20 +1,7 @@
+process.env.CHROME_BIN = require("puppeteer").executablePath();
+
 module.exports = function (config) {
   config.set({
-    basePath: "",
-    frameworks: ["jasmine", "@angular-devkit/build-angular"],
-    plugins: [
-      require("karma-jasmine"),
-      require("@angular-devkit/build-angular/plugins/karma"),
-    ],
-    client: {
-      clearContext: false,
-    },
-    reporters: ["progress"],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: false,
-    browsers: [ChromeHeadless],
-    singleRun: true,
+    browsers: ["ChromeHeadless"],
   });
 };
