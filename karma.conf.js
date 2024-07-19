b38159695f8d63caf9b98936915709fa00cad164
José Bocanegra <josejbocanegra@gmail.com>
2024-07-19 10:47:22 -0500

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -1,47 +1,37 @@
+process.env.CHROME_BIN = require('puppeteer').executablePath();
+console.log(process.env.CHROME_BIN)
 module.exports = function (config) {
   config.set({
-    basePath: "",
-    frameworks: ["jasmine", "@angular-devkit/build-angular"],
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
-      require("karma-jasmine"),
-      require("karma-mocha-reporter"),
-      require("karma-chrome-launcher"),
-      require("karma-jasmine-html-reporter"),
-      require("karma-coverage-istanbul-reporter"),
-      require("karma-sonarqube-unit-reporter"),
-      require("@angular-devkit/build-angular/plugins/karma"),
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
-      clearContext: false, // leave Jasmine Spec Runner output visible in browser
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    coverageIstanbulReporter: {
-      dir: require("path").join(__dirname, "./coverage/front"),
-      reports: ["html", "lcovonly", "text-summary"],
-      fixWebpackSourcePaths: true,
-    },
-    sonarQubeUnitReporter: {
-      sonarQubeVersion: "LATEST",
-      outputFile: "reports/ut_report.xml",
-      overrideTestDescription: true,
-      testPaths: ["./src"],
-      testFilePattern: ".spec.ts",
-      useBrowserName: false,
-    },
-    reporters: ["mocha", "kjhtml", "sonarqubeUnit", "coverage-istanbul"],
+    reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ["ChromeHeadless"],
+    browsers: ['Chrome', 'ChromeHeadlessNoSandbox'],
+    captureTimeout: 210000,
+    browserDisconnectTolerance: 3,
+    browserDisconnectTimeout: 210000,
+    browserNoActivityTimeout: 210000,
     customLaunchers: {
-      ChromeHeadless: {
-        base: 'Chrome',
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
         flags: [
-          '-headless',
+          '--no-sandbox',
+          '--remote-debugging-port=9222'
         ]
       }
     },
-    singleRun: true,
-    restartOnFileChange: true,
+    singleRun: false
   });
 };
